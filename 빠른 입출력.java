44180e699e5b6e7e8dc9c7b02b0a350d50c9cb42
yesung20 <155213489+yesung20@users.noreply.github.com>
2023-12-31 08:59:19 +0900

    Create 빠른 입출력.java

---
+++ 빠른 입출력.java
@@ -0,0 +1,19 @@
+package com.CodingTest;
+
+import java.io.*;
+import java.util.*;
+
+public class bj_15552 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        for(int i=0; i<n; i++){
+            String s = br.readLine();
+            int a = Integer.parseInt(s.split(" ")[0]);
+            int b = Integer.parseInt(s.split(" ")[0]);
+            bw.write(a+b+"\n");
+        }
+        bw.flush();
+    }
+}
